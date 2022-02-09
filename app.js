c85fbebb2c945d408c9f7d441eaf4387aed4c7a7
Hyesung Park <jayhspark@gmail.com>
2022-02-09 17:46:23 +0900

    update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express")
+const http = require('http')
 const path = require("path")
-
+const socketio = require('socket.io')
 const Port = process.env.Port || 3011
 
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const publicDirectoryPath = path.resolve(__dirname, "./public")
 
 app.use(express.static(publicDirectoryPath))
 
 
-app.listen(Port, () => console.log(`server is up on port ${Port}`))
+io.on('connnection', () =>{
+    console.log('New WebSokect connection')
+})
+
+server.listen(Port, () => console.log(`server is up on port ${Port}`))
